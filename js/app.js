02a674ebb40997904c2c90bc2712c3ecbbfa3c6e
david Maser <david.maser@altitude-sports.com>
2016-08-26 16:34:52 -0400

    Fooling around

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 /// <reference path="../typings/tsd.d.ts" />
-$(function () {
-});
+var getDataSets = {
+    initDataFlow: function (common) {
+        this.rowOutput = null;
+    }
+};
+var buildDataOutput = (function () {
+    function buildDataOutput(data, callback, utf) {
+        this.data = data;
+        this.callback = callback;
+        this.utf = utf;
+        this.dataBlock = $.merge(data, callback);
+    }
+    return buildDataOutput;
+}());
+new buildDataOutput('core.json', 'json', 'call this thing');
 //# sourceMappingURL=app.js.map
--- js/app.ts
+++ js/app.ts
@@ -1,4 +1,23 @@
 /// <reference path="../typings/tsd.d.ts" />
-$(function(){
+declare var DataStore: any;
+interface Common {
+    dataRow:number;
+    dataColumn:number;
+    cell:string;
+    selected:boolean;
+}
+var getDataSets = {
+    initDataFlow:function(common:Common){
+        this.rowOutput = null;
+      }
+};
+class buildDataOutput{
+    dataSource: string;
+    dataFormat:string;
+    dataBlock:any;
+    constructor(public data, public callback, public utf) {
+        this.dataBlock = $.merge(data,callback);
+    }
+}
 
-})
+new buildDataOutput('core.json','json','call this thing');
